b12205a1bee095a726fb1bbc14b2c84294a3df60
mohammad-adil <shahaadil13@gmail.com>
2020-10-05 16:01:22 +0530

    style changing

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -12,7 +12,7 @@ const forecast = (longitude, lattitude, callback) => {
         } else {
 
 
-            callback(undefined, 'it\'s currently  ' + response.body.current.temperature + ' \n The u-v Index is ' + response.body.current.uv_index + ' \n The chance of rain is ' + response.body.current.precip + ' \n The wind speed is: ' + response.body.current.wind_dir)
+            callback(undefined, 'it\'s currently  ' + response.body.current.temperature + ' degree Fahrenheit' + ' \n The u-v Index is ' + response.body.current.uv_index + ' \n The chance of rain is ' + response.body.current.precip + ' \n The wind speed is: ' + response.body.current.wind_dir)
 
         }
     })
